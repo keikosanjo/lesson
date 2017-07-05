07b6b806d080c2f6ba161fbdec7a2c925f3f52aa
keikosanjo <stt.0445@gmail.com>
2017-07-06 00:17:02 +0900

    made fibonacci list and modifided for loop

--- python/problem2.py
+++ python/problem2.py
@@ -1,8 +1,22 @@
-F = 1
-S = 2
+Sum = 0
+Fib = [1 , 2]
+max = 4000000
 
-for i in range(1 , 4000000):
+## フィボナッチ数列作成
+i = 1
+while (Fib[i] <= max):
+    if i == 1:
+        Fib[i] = 2
+    elif i == 2:
+        Fib.append(Fib[0] + Fib[1])
+    elif i == 3:
+        Fib.append(Fib[1] + Fib[2])
+    else:
+        Fib.append(Fib[i-1] + Fib[i-2])
+print(Fib)
 
-    Fib = F + S
-    F = S
-    S = Fib
+## 偶数値の項の総和
+for i in range(0, len(Fib) - 1):
+    if Fib[i] % 2 == 0:
+        Sum += Fib[i]
+print('偶数値の合計= %s' %Sum)
