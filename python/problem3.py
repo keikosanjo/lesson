c9f1ec66004385d2d7c9fe9a2c2f4852b144e19d
keikosanjo <stt.0445@gmail.com>
2017-09-06 21:50:08 +0900

    made the program to calculate the largest prime factor

--- python/problem3.py
+++ python/problem3.py
@@ -1,2 +1,13 @@
-## slacktest
+Number = 600851475143
+answer = 0
 
+for num in range(1, Number):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        #print(num)
+        if Number % num == 0:
+            answer = num
+print(answer)
