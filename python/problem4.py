db25a6165b92fef8315079e7837c290902529173
keikosanjo <stt.0445@gmail.com>
2017-09-11 21:18:21 +0900

    made the code calculate the maximum value of paindrome number

--- python/problem4.py
+++ python/problem4.py
@@ -1,6 +1,14 @@
-for num1 in range(100, 999):
-    for num2 in range(100, 999):
+list = []
+for num1 in range(100, 1000):
+    for num2 in range(100, 1000):
+       count = 0
        value = num1 * num2
        str_value = str(value)
-       for i in range(0,len(str_value)):
-           print( str_value[0] )
+       for i in range(0, int(len(str_value)/2)):
+           #print(value)
+           if(str_value[i] == str_value[len(str_value)-1-i]):
+               count += 1
+               if(count==3):
+                   list.append(value)
+list.sort()
+print(list[len(list)-1])
