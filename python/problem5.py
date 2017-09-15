638e042f2b212ed3ee402671583ecc941a88cadf
keikosanjo <stt.0445@gmail.com>
2017-09-15 23:52:23 +0900

    made the code calculate the minimum multiple

--- python/problem5.py
+++ python/problem5.py
@@ -1,8 +1,9 @@
 count = 0
-for num in range(1,100000):
+num = 0
+while count < 20:
+    num += 1
+    count = 0
     for i in range(1,21):
-        if((num/i) == 0):
+        if num % i == 0:
             count += 1
-            if(count == 20):
-                print(num)
-
+print(num)
