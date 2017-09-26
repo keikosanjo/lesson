1648f007e02e06407227730e9245c5333989d5c5
keikosanjo <stt.0445@gmail.com>
2017-09-26 21:57:50 +0900

    made the code of calculate the pythagoras number

--- python/problem9.py
+++ python/problem9.py
@@ -1,9 +1,11 @@
-a
-b
-c
-
-number = 1000
 def calculate_number_pythagoras(number):
-    a = (m*m)-(n*n)
-    b = 2*m*n
-    c = (m*m)+(n*n)
+    for m in range(1000):
+        for n in range(1000):
+            a = (m*m)-(n*n)
+            b = 2*m*n
+            c = (m*m)+(n*n)
+            if (a*a) + (b*b) == c*c:
+                if a + b + c == number and a >= 0 and b >= 0 and c >= 0:
+                    print(a*b*c)
+
+calculate_number_pythagoras(1000)
