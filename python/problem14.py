c5cfba9f1f20bee5f59accdd4a67e18eda03100b
keikosanjo <stt.0445@gmail.com>
2017-10-26 23:23:46 +0900

    made the code of problem14

--- python/problem14.py
+++ python/problem14.py
@@ -1,13 +1,22 @@
+list = []
 def collatz(n):
-    list = []
-    list.append(n)
-    while n == 1:
+    N = n
+    while n > 1:
         if n%2 == 0:
-            list.append(n%2)
-            n = n%2
+            list.append(int(n/2))
+            n = int(n/2)
         elif n%2 != 0:
             list.append((3*n)+1)
-            n = (3*n)+1
-    print(list)
-collatz(13)
+            n = ((3*n)+1)
+    list.insert(0, N)
 
+listLength = 0
+listValue = 0
+print(len(list))
+for x in range(0, 1000000):
+    collatz(x)
+    if len(list) >= listLength:
+        listLength = len(list)
+        listValue = x
+print('最長の数列を生成する値：{0}'.format(listValue))
+print('数列の長さ：{0}'.format(listLength))
