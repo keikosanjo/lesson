756f8d2e5f6c8acd6896e4f120563d487b3f625b
keikosanjo <stt.0445@gmail.com>
2018-01-01 20:16:37 +0900

    problem23

--- python/problem23.py
+++ python/problem23.py
@@ -5,6 +5,22 @@ def sum_of_true_divisor(number):
             total += i
     return(total)
 
-#print(sum_of_true_divisor(28))
+list = []
+def judge_sum_of_divisor(N):
+    for i in list:
+        if i > N/2:
+            break
+        if N-i in list:
+            return True
+    return False
+
 for i in range(1,28123):
     if sum_of_true_divisor(i) > i:
+        print(i)
+        list.append(i)
+
+total = 0
+for i in range(1,28123):
+    if judge_sum_of_divisor(i) == False:
+        total += i
+print(total)
