7bd8a9a143603aab1daaf994e4519c84cd637dd1
keikosanjo <stt.0445@gmail.com>
2018-01-28 23:15:40 +0900

    problem26

--- python/problem26.py
+++ python/problem26.py
@@ -1,18 +1,31 @@
 def calculate_reciprocal_number(d):
     array = []
-    x = 0
     i = 0
-    while x != 1:
+    x = 1%d
+    array.append(x)
+    loop = True
+    while(loop):
         x = (array[i]*10)%d
-        array.append(x)
+        if x == 0:
+            array.clear()
+            array.append(0)
+            break
+        elif x != 1:
+            if x == array[i-1]:
+                array.clear()
+                array.append(0)
+                break
+            else:
+                array.append(x)
+        elif x == 1:
+            break
         i += 1
-    return(int(len(array)-1))
+    return(int(len(array)))
 
 digit_number = []
-for d in range(1,8):
-    a = calculate_reciprocal_number(d)
-    digit_number.append(a)
+for d in range(1,1000):
+    digit_number.append(calculate_reciprocal_number(d))
 number = max(digit_number)
 d =  digit_number.index(number)
-print('d:{0}, number of digits:{1}',format(d,number))
-
+answer = d+1
+print(answer)
